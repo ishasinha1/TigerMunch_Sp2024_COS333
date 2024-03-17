3c41adf9a8bf61efc11ee94ee8d5d10f11f2b961
Jeremiah Giordani <jeremiah@dynamic-oit-vapornet100-10-9-125-178.princeton.edu>
2024-03-17 10:38:17 -0400

    API tweak pt 3

--- request_handler.py
+++ request_handler.py
@@ -6,7 +6,7 @@ import pillow_heif
 import io
 import base64
 
-testing_api = True
+testing_api = False
 
 def strip_json(message_content):
     content_string = message_content["choices"][0]["message"]["content"]
