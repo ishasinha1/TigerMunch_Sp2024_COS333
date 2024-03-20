748a270870f6b160d96651a35f5189ebb17206bf
AylinHad <ah4068@princeton.edu>
2024-03-20 14:55:30 -0400

    fixed modal to work with the database

--- access_data.py
+++ access_data.py
@@ -20,16 +20,16 @@ def fetch_all_data():
             cursor.execute("SELECT * FROM user_inputs WHERE username = %s ORDER BY created_at DESC", (username,))
             return cursor.fetchall()
 
-def get_user_data(method, first_name, last_name, cal_goal, fat_goal, protein_goal, carb_goal):
+def get_user_data():
     username = auth.authenticate()
     with psycopg2.connect(_DATABASE_URL) as connection:
         with connection.cursor() as cursor:
             cursor.execute("SELECT * FROM user_data WHERE username = %s", (username,))
             table = cursor.fetchall()
 
-
+    print("getting user data")
     if table:
-        account_info_dict = {
+        return {
             'first_name': table[0][2],
             'last_name': table[0][3],
             'calorie_goal': table[0][4],
@@ -38,7 +38,7 @@ def get_user_data(method, first_name, last_name, cal_goal, fat_goal, protein_goa
             'carb_goal': table[0][7],
         }
     else:
-        account_info_dict = {
+        return  {
             'first_name': '',
             'last_name': '',
             'calorie_goal': -1,
@@ -47,43 +47,9 @@ def get_user_data(method, first_name, last_name, cal_goal, fat_goal, protein_goa
             'carb_goal': -1,
         }
 
-    print(cal_goal)
-    if first_name is not None:
-        account_info_dict['first_name'] = first_name
-    if last_name is not None:
-        account_info_dict['last_name'] = last_name
-
-    if cal_goal is not None:
-        print("changed CALORIES!!")
-        if cal_goal == '':
-            account_info_dict['calorie_goal'] = -1
-        else:
-            account_info_dict['calorie_goal'] = cal_goal
-    if fat_goal is not None:
-        if fat_goal == '':
-            account_info_dict['fat_goal'] = -1
-        else:
-            account_info_dict['fat_goal'] = fat_goal
-
-    if protein_goal is not None:
-        if protein_goal == '':
-            account_info_dict['protein_goal'] = -1
-        else:
-            account_info_dict['protein_goal'] = protein_goal 
-
-    if carb_goal is not None:
-        if carb_goal == '':
-            account_info_dict['carb_goal'] = -1
-        else:
-            account_info_dict['carb_goal'] = carb_goal 
-    if method == 'POST':
-        print('access data', method)
-        insert_user_data(account_info_dict['first_name'], account_info_dict['last_name'], account_info_dict['calorie_goal'], account_info_dict['fat_goal'], account_info_dict['protein_goal'], account_info_dict['carb_goal'])
-
-    return account_info_dict
-
 def insert_user_data(first_name, last_name, cal_goal, fat_goal, protein_goal, carb_goal):
     username = auth.authenticate()
+    print("inserting user data")
 
     with psycopg2.connect(_DATABASE_URL) as connection:
         with connection.cursor() as cursor:
