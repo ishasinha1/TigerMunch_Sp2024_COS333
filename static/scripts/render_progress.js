736466d34fb1dbb08e7fe937e748c57df2d2f57f
ishasinha1 <113146215+ishasinha1@users.noreply.github.com>
2024-03-26 16:21:35 -0400

    modal submit WIP

---
+++ static/scripts/render_progress.js
@@ -0,0 +1,90 @@
+function renderNutritionChart(canvasId, dailyTotal, goal, label, units) {
+    'use strict';
+    if (goal == -1) {
+        // Hide the chart and label for this macro counter
+        document.getElementById(`${canvasId}Container`).style.display = 'none';
+
+        return; // Exit the function early
+    }
+
+    const ctx = document.getElementById(canvasId).getContext('2d');
+    const percentageDisplayId = `${canvasId}Percentage`;
+    let backgroundColors;
+    let dataValues;
+    
+    if (dailyTotal <= goal) {
+        // If daily intake is less than or equal to the goal
+        backgroundColors = ['green', 'grey'];
+        dataValues = [dailyTotal, goal - dailyTotal];
+    } else if(dailyTotal <= goal * 2){;
+        backgroundColors = ['orange', 'green'];
+        dataValues = [dailyTotal - goal, 2 *goal - dailyTotal];
+    } else if(dailyTotal <= goal * 3){
+        backgroundColors = ['red', 'orange'];
+        dataValues = [dailyTotal - 2*goal, 3 *goal - dailyTotal];
+    } else{
+        backgroundColors = ['red', 'grey'];
+        dataValues = [1, 0];
+    }
+
+    const data = {
+        datasets: [{
+            data: dataValues,
+            backgroundColor: backgroundColors,
+            borderWidth: 0
+        }]
+    };
+    
+    const config = {
+        type: 'doughnut',
+        data: data,
+        options: {
+            cutout: '75%',
+            // rotation: -90,
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+    };
+    
+    new Chart(ctx, config);
+    
+    // Update the percentage text and label
+    const percentage = (dailyTotal / goal) * 100;
+    document.getElementById(percentageDisplayId).innerText = `${label}: ${percentage.toFixed(2)}%`;
+    
+    // Update the label text
+    const labelDisplayId = `${canvasId}Label`;
+    document.getElementById(labelDisplayId).innerText = `${dailyTotal.toFixed(2)} ${units} / ${goal.toFixed(2)} ${units}`;
+}
+
+async function render_chart() {
+    // const ctx = document.getElementById('caloriesChart').getContext('2d');
+    const response = await fetch('/nutrition');
+    const database_values = await response.json();
+
+    const caloriesGoal = Number(database_values.calorie_goal); // received from the backend
+    const dailyCalorieCount = Number(database_values.calories);
+
+    renderNutritionChart('caloriesChart', dailyCalorieCount, caloriesGoal, "Calories", "cal")
+    
+    const fatGoal = Number(database_values.fat_goal); // received from the backend
+    const dailyFatCount = Number(database_values.fat);
+    renderNutritionChart('fatChart', dailyFatCount, fatGoal, "Fat", "g")
+
+    const proteinGoal = Number(database_values.protein_goal); // received from the backend
+    const dailyProteinCount = Number(database_values.protein);
+    renderNutritionChart('proteinChart', dailyProteinCount, proteinGoal, "Protein", "g")
+
+    const carbGoal = Number(database_values.carb_goal); // received from the backend
+    const dailyCarbCount = Number(database_values.carbs);
+    renderNutritionChart('carbChart', dailyCarbCount, carbGoal, "Carb", "g")  
+}
+
+document.addEventListener('DOMContentLoaded', render_chart);
+
+// update_account_settings = document.getElementById('account-update');
+
+// update_account_settings.onclick() = render_chart();
