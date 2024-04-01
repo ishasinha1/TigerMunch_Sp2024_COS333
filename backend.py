64af53cc5d12d06415d341d66ad8f632bad5fc61
AylinHad <ah4068@princeton.edu>
2024-03-31 21:48:25 -0400

    pulling changes back, will be put in the branch, only left is the pop-up block landing page

--- backend.py
+++ backend.py
@@ -178,19 +178,6 @@ def insert():
     
 @app.route('/contact_us', methods=['GET', 'POST'])
 def contact_us():
-    if request.method == 'POST':
-        # Extract the data from the request
-        data = request.json
-        name = data['name']
-        email = data['email']
-        message = data['message']
-
-        # Now, save this data to a feedback.txt file or handle as needed
-        with open('feedback.txt', 'a') as file:
-            file.write(f"Name: {name}, Email: {email}, Message: {message}\n")
-
-        # Respond with a success message
-        return jsonify({'message': 'Feedback received successfully!'})
     if auth.signin() is None:
         login_url = (_CAS_URL + 'login?service=' +
           urllib.parse.quote(flask.request.url))
