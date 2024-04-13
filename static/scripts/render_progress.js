c3acdf529c2b59c74f25add978e79308d6291c7c
Jeremiah Giordani <jeremiah@dynamic-oit-ip4-wifirestricted03-10-48-27-220.princeton.edu>
2024-04-13 15:55:43 -0400

    description fixes

--- static/scripts/render_progress.js
+++ static/scripts/render_progress.js
@@ -95,7 +95,7 @@ async function render_description() {
     // const ctx = document.getElementById('caloriesChart').getContext('2d');
     const response = await fetch('/description');
     const response_text = await response.text();
-    document.getElementById('description').innerText = response_text;
+    document.getElementById('description_of_trends').innerText = response_text;
 }
 
 
