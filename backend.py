114a0ff72469294124b2ad720530cb57d5d0e3c1
ishasinha1 <113146215+ishasinha1@users.noreply.github.com>
2024-04-19 14:36:03 -0400

    error page + idek styles

--- backend.py
+++ backend.py
@@ -100,11 +100,11 @@ def save_results():
 
 @app.route('/get_results', methods=['GET', 'POST'])
 def get_results():
+    username = auth.authenticate()
     try:
         if request.method == 'POST':
             photo = request.files.get('photo')
             description = request.form.get('description')
-            username = auth.authenticate()
             if photo and description:
                 print("running multi-modal")
                 calorie_estimate, fat_estimate, protein_estimate, \
@@ -118,24 +118,23 @@ def get_results():
                 calorie_estimate, fat_estimate, protein_estimate, \
                 carb_estimate = language_module.handle_description(description)
             else:
-                # return render_template('no_input.html', username=username)
                 return 'No input provided', 400 
 
             # time.sleep(5)
             
             # access_data.insert_meal(calorie_estimate, fat_estimate, protein_estimate, carb_estimate)
             if not all(isinstance(x, int) for x in [calorie_estimate, fat_estimate, protein_estimate, carb_estimate]):
-                return render_template('error.html')
+                return render_template('error.html', username=username)
 
             if calorie_estimate == 0 and fat_estimate == 0 and protein_estimate == 0 and carb_estimate == 0:
-                return render_template('error.html')
+                return render_template('error.html', username=username)
             
             return render_template('display_output.html', \
                 calorie_estimate=calorie_estimate, fat_estimate=fat_estimate,\
                 protein_estimate=protein_estimate, carb_estimate=carb_estimate,\
                 username=username)
     except Exception as e:
-        return render_template('error.html')
+        return render_template('error.html', username=username)
 
 @app.route('/get_summary', methods=['GET', 'POST'])
 def get_summary():
