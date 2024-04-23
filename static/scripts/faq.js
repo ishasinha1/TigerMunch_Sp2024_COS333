bdfba0210fa466d7903c940b51ca8ec13d3f5dad
AylinHad <ah4068@princeton.edu>
2024-04-23 19:05:41 -0400

    image upload

---
+++ static/scripts/faq.js
@@ -0,0 +1,17 @@
+var buttonclick = document.getElementsByClassName("box");
+var i;
+
+for (i = 0; i < buttonclick.length; i++) {
+  buttonclick[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    this.parentElement.classList.toggle("active");
+
+    var pannel = this.nextElementSibling;
+
+    if (pannel.style.display === "block") {
+      pannel.style.display = "none";
+    } else {
+      pannel.style.display = "block";
+    }
+  });
+}
--- static/scripts/file_upload.js
+++ static/scripts/file_upload.js
@@ -2,6 +2,21 @@ const btn = document.getElementById('photo');
 
 const fileChosen = document.getElementById('file-chosen');
 
-btn.addEventListener('change', function(){
-    fileChosen.textContent = this.files[0].name
-  })
+const photoPreview = document.getElementById('photo-preview');
+  
+  btn.addEventListener('change', function() {
+    if (this.files && this.files[0]) {
+      fileChosen.textContent = this.files[0].name;
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        photoPreview.src = e.target.result;
+        fileChosen.style.display = 'none'
+        photoPreview.style.display = 'block'; 
+      };
+      reader.readAsDataURL(this.files[0]); 
+    } else {
+      fileChosen.textContent = 'No file chosen';
+      photoPreview.style.display = 'none'; 
+    }
+  });
+  
