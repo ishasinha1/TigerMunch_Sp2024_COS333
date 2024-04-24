fe042d826fc4a14a2a09f5222059934dd40e2211
AylinHad <ah4068@princeton.edu>
2024-04-24 11:47:04 -0400

    image preview

--- static/scripts/file_upload.js
+++ static/scripts/file_upload.js
@@ -2,21 +2,21 @@ const btn = document.getElementById('photo');
 
 const fileChosen = document.getElementById('file-chosen');
 
-const photoPreview = document.getElementById('photo-preview');
+const photoPreview = document.getElementById('preview');
   
   btn.addEventListener('change', function() {
     if (this.files && this.files[0]) {
       fileChosen.textContent = this.files[0].name;
-      const reader = new FileReader();
-      reader.onload = function(e) {
-        photoPreview.src = e.target.result;
-        fileChosen.style.display = 'none'
-        photoPreview.style.display = 'block'; 
-      };
-      reader.readAsDataURL(this.files[0]); 
+      // const reader = new FileReader();
+      // reader.onload = function(e) {
+      //   photoPreview.src = e.target.result;
+        // fileChosen.style.display = 'none'
+        preview.style.display = 'block'; 
+      // };
+      // reader.readAsDataURL(this.files[0]); 
     } else {
       fileChosen.textContent = 'No file chosen';
-      photoPreview.style.display = 'none'; 
+      preview.style.display = 'none'; 
     }
   });
   
