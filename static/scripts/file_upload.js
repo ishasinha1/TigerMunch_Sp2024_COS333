b2729e4478db1511069118847ce2752fdbd49c50
AylinHad <ah4068@princeton.edu>
2024-04-24 14:20:43 -0400

    image upload fixed?

--- static/scripts/file_upload.js
+++ static/scripts/file_upload.js
@@ -3,20 +3,9 @@ const btn = document.getElementById('photo');
 const fileChosen = document.getElementById('file-chosen');
 
 const photoPreview = document.getElementById('preview');
-  
-  btn.addEventListener('change', function() {
-    if (this.files && this.files[0]) {
-      fileChosen.textContent = this.files[0].name;
-      // const reader = new FileReader();
-      // reader.onload = function(e) {
-      //   photoPreview.src = e.target.result;
-        // fileChosen.style.display = 'none'
-        preview.style.display = 'block'; 
-      // };
-      // reader.readAsDataURL(this.files[0]); 
-    } else {
-      fileChosen.textContent = 'No file chosen';
-      preview.style.display = 'none'; 
-    }
-  });
+
+btn.addEventListener('change', function(){
+    fileChosen.textContent = this.files[0].name
+    preview.style.display = 'block'; 
+  })
   
