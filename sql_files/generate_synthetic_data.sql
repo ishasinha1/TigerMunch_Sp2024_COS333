33acb5c5d5428513d014709f5af90ab57bdd32cd
Jeremiah Giordani <jeremiah@dynamic-oit-ip4-wifirestricted03-10-48-27-220.princeton.edu>
2024-04-30 16:16:28 -0400

    sql change

--- sql_files/generate_synthetic_data.sql
+++ sql_files/generate_synthetic_data.sql
@@ -2,13 +2,13 @@
 DO $$
 DECLARE
     i int := 0;
-    entry_date date := '2024-04-30'; -- starting date
+    entry_date date := '2024-05-01'; -- starting date
 BEGIN
-    WHILE i < 35 LOOP
+    WHILE i < 50 LOOP
         INSERT INTO user_inputs (username, calories, fat, protein, carbs, created_at)
         VALUES (
             'is4684',
-            floor(random() * (3000 - 1500 + 1) + 1500), -- Random calories between 1500 and 3000
+            floor(random() * (2500 - 1500 + 1) + 1500), -- Random calories between 1500 and 3000
             floor(random() * (300 - 100 + 1) + 100),    -- Random fat between 100 and 300
             floor(random() * (300 - 100 + 1) + 100),    -- Random protein between 100 and 300
             floor(random() * (300 - 100 + 1) + 100),    -- Random carbs between 100 and 300
